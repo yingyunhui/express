bc32cec63417d2c2954baa441258bde88be2557c
yingyunhui <yingyunhui@hotmail.com>
2017-08-03 19:03:48 +0800

    add cookie

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var app = express();
 
 var index = require('./routes/index');
 var api = require('./routes/api');
 
+var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 //app.use(express.static('www'));
 app.use("/",index);
 app.use("/api",api);

913a375cdddd2a5f0d77f9d363acc616c7f5a63c
yingyunhui <yingyunhui@hotmail.com>
2017-08-03 14:09:27 +0800

    add router

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-app.use(express.static('www'));
+var index = require('./routers/index');
+var api = require('./routers/api');
 
-app.get('/api', function (req, res) {
-  res.send('ok');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(express.static('www'));
+app.use("/",index);
+app.use("/api",api);
 
 var server = app.listen(8080, function () {
   console.log('success on 8080');
